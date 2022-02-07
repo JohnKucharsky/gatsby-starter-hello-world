0dad7a914523b625a0000ba28ae314d4c04f7ea5
Johnkuchar <vannok@live.com>
2022-02-07 04:39:45 +0300

    3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,3 +1,4 @@
+import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
 
